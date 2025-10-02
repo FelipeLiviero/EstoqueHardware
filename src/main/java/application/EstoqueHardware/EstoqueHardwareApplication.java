b8a8cec13a40022cd565db70a684f86c34987b51
FelipeLiviero <felipe.liviero@uel.br>
2025-10-01 22:44:00 -0300

    Recomecando

---
+++ src/main/java/application/EstoqueHardware/EstoqueHardwareApplication.java
@@ -0,0 +1,13 @@
+package application.EstoqueHardware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueHardwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueHardwareApplication.class, args);
+	}
+
+}
